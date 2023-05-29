7aa513f3593aa706424c3aa5cd3959f5c8903a12
george <igeorgerome@gmail.com>
2023-05-29 14:57:12 +0300

    created a button lol

--- lovepaws/src/pages/Home.js
+++ lovepaws/src/pages/Home.js
@@ -1,11 +1,24 @@
 import React from 'react';
-
+import Nav from '../components/Nav';
 const Home = () => {
+
+    const authToken=false
+    const handleClick=()=>{
+        console.log('Clicked')
+    }
+
   return (
-    <div>
-      Home
+    <>
+    <Nav/>
+    <div className="home">
+      <h1>Welcome to the Pet Matching App</h1>
+      <p>Swipe left or right to find walking partners for your pets!</p>
+      <button className='primary-button' onClick={handleClick}>
+        {authToken ? 'Signout':'Create Account' }
+      </button>
     </div>
+    </>
   );
-}
+};
 
 export default Home;
